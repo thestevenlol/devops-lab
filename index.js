bdfc72cc39fd03434c29c4915f06b3b6585470cf
Jack Foley <jackfdevelopment@gmail.com>
2026-06-26 00:34:01 +0100

    add some logic

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const http = require('http');
+const app = require('./app');
+
 const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Docker\n');
-});
-
-server.listen(port, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`listening on 0.0.0.0:${port}`);
 });
